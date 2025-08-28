e3da9d682b23611d18d4e23b660effa2c71a37a4
Esraa Darwish <esraadarwish107@gmail.com>
2025-08-28 23:33:32 +0300

    assets

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -26,19 +26,59 @@ class Assets {
 
   static const String assetsImagesGoogle = "assets/images/google.png";
 
+  static const String assetsImagesHome = "assets/images/home.png";
+
+  static const String assetsImagesHomeactive = "assets/images/homeactive.png";
+
   static const String assetsImagesImage11 = "assets/images/image11.png";
 
   static const String assetsImagesJega = "assets/images/jega.png";
 
-  static const String assetsImagesMael2 = "assets/images/mael2.png";
-
   static const String assetsImagesMeal1 = "assets/images/meal1.png";
+
+  static const String assetsImagesMeal2 = "assets/images/meal2.png";
+
+  static const String assetsImagesMeal3 = "assets/images/meal3.png";
+
+  static const String assetsImagesMeal4 = "assets/images/meal4.png";
+
+  static const String assetsImagesMeal5 = "assets/images/meal5.png";
+
+  static const String assetsImagesNotification = "assets/images/notification.png";
+
+  static const String assetsImagesNotificationactive = "assets/images/notificationactive.png";
+
+  static const String assetsImagesPerson = "assets/images/person.png";
+
+  static const String assetsImagesPerson2 = "assets/images/person2.png";
+
+  static const String assetsImagesProfile = "assets/images/profile.png";
+
+  static const String assetsImagesProfileactive = "assets/images/profileactive.png";
+
+  static const String assetsImagesRatingfilled = "assets/images/Ratingfilled.png";
 
   static const String assetsImagesRecipe = "assets/images/recipe.png";
 
+  static const String assetsImagesRecipe2 = "assets/images/recipe2.png";
+
+  static const String assetsImagesRecipe3 = "assets/images/recipe3.png";
+
+  static const String assetsImagesRecipe4 = "assets/images/recipe4.png";
+
+  static const String assetsImagesRecipe5 = "assets/images/recipe5.png";
+
   static const String assetsImagesRectangle6 = "assets/images/Rectangle6.png";
 
+  static const String assetsImagesSaved = "assets/images/saved.png";
+
+  static const String assetsImagesSavedactive = "assets/images/savedactive.png";
+
+  static const String assetsImagesStar = "assets/images/Star.png";
+
   static const String assetsImagesTaco = "assets/images/taco.png";
+
+  static const String assetsImagesTimer = "assets/images/timer.png";
 
   static const String assetsImagesTomato = "assets/images/tomato.png";
 }
